988d8926794b3faf11fd13ef9bfc0907bfaaa967
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-04-06 12:45:55 +0700

    create register activity

---
+++ app/src/main/java/com/example/tugas1_akb_if9_10118377/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.tugas1_akb_if9_10118377;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
