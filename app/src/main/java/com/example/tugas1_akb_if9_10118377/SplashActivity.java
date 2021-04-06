38b64c73da5ebecd63f4965cb67b2f308decac01
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-04-06 11:22:26 +0700

    create splash screen activity

---
+++ app/src/main/java/com/example/tugas1_akb_if9_10118377/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.example.tugas1_akb_if9_10118377;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
