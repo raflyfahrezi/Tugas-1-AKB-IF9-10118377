d26c3e31e7777064b59dddc226899e064ed5b27c
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-04-06 13:05:17 +0700

    create intent to login

--- app/src/main/java/com/example/tugas1_akb_if9_10118377/RegisterActivity.java
+++ app/src/main/java/com/example/tugas1_akb_if9_10118377/RegisterActivity.java
@@ -2,9 +2,20 @@ package com.example.tugas1_akb_if9_10118377;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+    TextView loginText;
+
+    // Intent to Login
+    private void toLogin() {
+        Intent intentToLogin = new Intent(RegisterActivity.this, LoginActivity.class);
+
+        startActivity(intentToLogin);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +24,15 @@ public class RegisterActivity extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
+        // Initialize
+        loginText = findViewById(R.id.loginText);
+
+        // Login Text Handler onClick
+        loginText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toLogin();
+            }
+        });
     }
 }
