52c971422db7bde12bf3350d402ff571a38b81dd
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-04-06 11:46:07 +0700

    hide action bar

--- app/src/main/java/com/example/tugas1_akb_if9_10118377/SplashActivity.java
+++ app/src/main/java/com/example/tugas1_akb_if9_10118377/SplashActivity.java
@@ -13,6 +13,8 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        getSupportActionBar().hide();
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
